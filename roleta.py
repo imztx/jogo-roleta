fb864c3631b73f5d2c9de6e4134c7580c129ed7f
Murilo Silva Rêgo <murilosrego@gmail.com>
2024-04-03 22:54:53 +0000

    copiei colei

---
+++ roleta.py
@@ -0,0 +1,100 @@
+from typing import Sequence
+from dataclasses import dataclass
+
+DEFAULT_NUMBERS = (
+    0,
+    32,
+    15,
+    19,
+    4,
+    21,
+    2,
+    25,
+    17,
+    34,
+    6,
+    27,
+    13,
+    36,
+    11,
+    30,
+    8,
+    23,
+    10,
+    5,
+    24,
+    16,
+    33,
+    1,
+    20,
+    14,
+    31,
+    9,
+    22,
+    18,
+    29,
+    7,
+    28,
+    12,
+    35,
+    3,
+    26,
+)
+
+DEFAULT_RED_NUMBERS = tuple(DEFAULT_NUMBERS[1::2])
+DEFAULT_BLACK_NUMBERS = tuple(DEFAULT_NUMBERS[2::2])
+
+
+@dataclass
+class Bet:
+    value: float
+    numbers: Sequence[int]
+
+    @classmethod
+    def create_black(cls, value: float):
+        return cls(value=value, numbers=set(DEFAULT_BLACK_NUMBERS))
+
+    @classmethod
+    def create_red(cls, value: float):
+        return cls(value=value, numbers=set(DEFAULT_RED_NUMBERS))
+
+    @classmethod
+    def create_from_filter(cls, value: float, fn):
+        return cls(
+            value=value,
+            numbers=set(filter(fn, DEFAULT_NUMBERS[1:]))
+        )
+
+    @classmethod
+    def create_odd(cls, value: float):
+        return cls.create_from_filter(value, lambda it: it % 2 == 1)
+
+    @classmethod
+    def create_even(cls, value: float):
+        return cls.create_from_filter(value, lambda it: it % 2 == 0)
+    
+    @classmethod
+    def create_interval(cls, value: float, begin: int, end: int):
+        return cls.create_from_filter(value, lambda it: begin <= it <= end)
+    
+    @classmethod
+    def create_quad(cls, value: float, begin: int):
+        if begin % 3 != 0 and begin < 34:
+            return cls(value,{ begin, begin + 1, begin +3, begin + 4 })
+        #raise Exception ('Aposta inválida')
+    
+    """ @classmethod
+    def create_duo(cls, value: float, duo: str):
+        begin, end = (duo.split(',')), (duo.split(','))
+        begin.sort(), begin.pop()
+        begin = int(begin[0])
+        end.sort(), end.pop(0)
+        end = int(end[0])
+        if (begin % 3 != 0 and 0 < begin < 36 and (end - begin == 1 or end - begin == 3)) or (begin % 3 == 0 and end % 3 == 0 and 0 < begin < 36 and end - begin == 3):
+            return cls(value, {begin, end})
+        raise Exception ('Aposta inválida') """
+    
+    
+
+class Roleta:
+    pass
---
+++ teste.py
@@ -0,0 +1,89 @@
+from unittest import TestCase
+from roleta import Bet
+from itertools import chain
+
+""" def all_2_horizontal():
+    for l in range(3):
+        yield from zip(range(1 + l, 32 + l, 3), range(4 + l, 35 + l, 3))
+        
+def all_2_vertical():
+    for c in range(0, 35, 3):
+        yield c + 1, c + 2
+        yield c + 2, c + 3
+        
+def all_2():
+    return chain(all_2_horizontal(), all_2_vertical()) """
+
+class RoletaTest(TestCase):
+
+    def test_run_test(self):
+        self.assertTrue(True)
+
+    def test_black(self):
+        bet = Bet.create_black(value=100.)
+        self.assertIsInstance(bet, Bet)
+        self.assertEqual(bet.value, 100.)
+        black = {
+            2, 4, 6, 8, 10, 11, 13, 15,
+            17, 20, 22, 24, 26, 28, 29, 31, 33, 35
+        }
+        self.assertSetEqual(bet.numbers, black)
+
+    def test_red(self):
+        bet = Bet.create_red(value=100.)
+        self.assertIsInstance(bet, Bet)
+        self.assertEqual(bet.value, 100.)
+        red = {
+            1, 3, 5, 7, 9, 12,
+            14, 16, 18, 19,
+            21, 23, 25, 27,
+            30, 32, 34, 36
+        }
+        self.assertSetEqual(bet.numbers, red)
+
+    def test_odd(self):
+        bet = Bet.create_odd(value=100.)
+        self.assertIsInstance(bet, Bet)
+        self.assertEqual(bet.value, 100.)
+        self.assertEqual(len(bet.numbers), 18)
+        self.assertEqual(len(list(filter(lambda it: it % 2 == 0, bet.numbers))), 0)
+
+    def test_even(self):
+        bet = Bet.create_even(value=100.)
+        self.assertIsInstance(bet, Bet)
+        self.assertEqual(bet.value, 100.)
+        self.assertEqual(len(bet.numbers), 18)
+        self.assertEqual(len(list(filter(lambda it: it % 2 == 1, bet.numbers))), 0)
+        
+    def test_interval_1_to_18(self):
+        bet = Bet.create_interval(value=100, begin=1, end=18)
+        self.assertEqual(bet.value, 100.)
+        self.assertEqual(len(bet.numbers), 18)
+        self.assertEqual(list(filter(lambda it: it < 19, bet.numbers)), list(it for it in range(1, 19)))
+        
+    def test_interval_13_to_24(self):
+        bet = Bet.create_interval(value=100, begin=13, end=24)
+        self.assertEqual(bet.value, 100)
+        self.assertEqual(len(bet.numbers), 12)
+        self.assertEqual(list(filter(lambda it: 12 < it < 25, bet.numbers)), list(it for it in range(13, 25)))
+        
+    def test_quad(self):
+        bet = Bet.create_quad(value=100, begin=14)
+        self.assertEqual(bet.value, 100)
+        self.assertEqual(len(bet.numbers), 4)
+        self.assertEqual((bet.numbers), {14,15,17,18})
+        
+    def test_duo_correct(self):
+        pass
+        """ dataset = dict(
+            (','.join(map(str, numbers)), set(numbers))
+            for numbers in all_2()
+        )
+        for input, expected in dataset.items():
+            with self.subTest(f"Convert {input} to {expected}"):
+                bet = self.create_duo(0, input)
+                self.assertIsInstance(bet, Bet)
+                self.assertEqual(bet.numbers, expected) """
+
+                    
+
