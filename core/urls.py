2b659323e088e59282a28b7866c8c45899f13104
MuriloSilvaRego <murilosrego@gmail.com>
2024-04-10 21:04:25 -0300

    Revert "teste django"
    This reverts commit 0c878e37e31621c0562d42377ce305546399c38b.

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,11 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- polls/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Jogo, Joinha
-
-admin.site.register(Jogo)
-admin.site.register(Joinha)
